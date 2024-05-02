fa599794f66c8e4fceddca1350e4d7301166f2b2
SawadaSyahmi <86639833+SawadaSyahmi@users.noreply.github.com>
2024-05-02 08:52:42 +0800

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,11 @@
+// You are given a set of code that count up when we click the floating action button
+// the client need the counter model to be shared across the applications using inherited widget
+// To make it worse, the counter is not updating when we click the button
+// Your task is to fix the code
+// You have one hour to complete this task
+// All the best
+
+
 import 'package:flutter/material.dart';
 import 'inherited.dart';
 
